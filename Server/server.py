432f123e6a2aa547bb8b4926311bb08ca24a2283
Resbi <resbi@redstone.link>
2018-02-04 11:51:48 +0000

    Crypto system

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,5 @@
 import socket,time,random,_thread,getre
-
+global temp_message
 
 def ding(conn):
     while 1:
@@ -7,7 +7,10 @@ def ding(conn):
         conn.send("'Server:Keep online.....'".encode(encoding='utf-8'))
 
 def check(conn,addr,Username):
+    global temp_message
+    tt = ""
     while 1:
+        """
         f = open("./Temps/temp.txt","r")
         a = f.read()
         f.close
@@ -21,8 +24,12 @@ def check(conn,addr,Username):
             f.write(a)
             f.close()
             conn.send(a.encode(encoding='utf-8'))
+        """
+        if tt != temp_message:
+            conn.send(temp_message.encode(encoding='utf-8'))
+            tt = temp_message
 
-wait = """
+"""
 def Logs(conn,addr,Username):
     while 1:
         Receive = conn.recv(1024)
@@ -30,7 +37,7 @@ def Logs(conn,addr,Username):
         log = open("log.txt","at")
         log.write("["+str(time.time())+"]"+"{"+str(addr)+"}"+Username+":"+str(Receive)+"\n")
         log.close()
-                
+     
         f = open("./Temps/temp.txt","w+")
         f.write("["+str(time.time())+"]"+Username+":"+str(Receive))
         f.close()
@@ -39,12 +46,13 @@ def Logs(conn,addr,Username):
 """
 
 def ChildThread(ip,port,Username,old):
+    global temp_message
     S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         S.bind((ip,port))
     except:
         print("Something error in port:" + str(port) + "let's try again.")
-        newport = int(random.random() * 10000)
+        newport = random.randint(2000,3000)
         ChildThread,(ip,newport,Username,old)
     S.listen(5)
     old.send(str(port).encode(encoding='utf-8'))
@@ -54,9 +62,12 @@ def ChildThread(ip,port,Username,old):
     print(addr)
 
     conn.settimeout(16384)
+    """
     f = open("./Temps/temp.txt","w+")
     f.write("["+str(time.time())+"]"+Username+":"+"Join the server!")
     f.close()
+    """
+    temp_message = "["+str(time.time())+"]"+Username+":"+"Join the server!"
 
 #    _thread.start_new_thread(Logs,(conn,addr,Username,))  #Save to log.
 
@@ -71,12 +82,13 @@ def ChildThread(ip,port,Username,old):
             log = open("log.txt","at")
             log.write("["+str(time.time())+"]"+"{"+str(addr)+"}"+Username+":"+str(Receive)+"\n")
             log.close()
-                
+            """    
             f = open("./Temps/temp.txt","w+")
             f.write("["+str(time.time())+"]"+Username+":"+str(Receive))
             f.close()
-
-            print("["+str(time.time())+"]"+"{"+str(addr)+"}"+Username+":"+str(Receive))
+            """
+            temp_message = "["+str(time.time())+"]"+Username+":"+Receive.decode("UTF-8")
+            print(temp_message)
 
 def GiveNewPort(ip,port):
     while 1:
@@ -98,12 +110,12 @@ def GiveNewPort(ip,port):
                 log = open("log.txt","at")
                 log.write("["+str(time.time())+"]"+"{"+str(addr)+"}"+"User:"+Username+":"+"Join the server!"+"\n")
                 log.close()
-
+                """
                 f = open("./Temps/%s.txt" % Username,"w+")
                 f.close()
-
+                """
 				
-                newport = int(random.random() * 10000)
+                newport = random.randint(2000,3000)
 
                 _thread.start_new_thread(ChildThread,(ip,newport,Username,conn))
 
@@ -112,6 +124,7 @@ def GiveNewPort(ip,port):
                 print("["+str(time.time())+"]"+"{"+str(addr)+"}"+Username)    #+"--port:"+str(newport))
 
                 break
+        S.close()
 
 
 ip = "0.0.0.0"
